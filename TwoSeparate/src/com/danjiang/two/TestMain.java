33dc2589e2ee92dc1775e3568577049cc3ebcdc6
Administrator <Administrator@MS-20110711NYGU>
2016-06-12 00:03:48 +0800

    [创建项目]

---
+++ TwoSeparate/src/com/danjiang/two/TestMain.java
@@ -0,0 +1,15 @@
+package com.danjiang.two;
+/**
+ * 
+ * Author: ZhangDanJiang
+ * 
+ * Date:2016年6月12日 Time:上午12:03:06
+ * 
+ * Function:
+ * 
+ */
+public class TestMain {
+	public static void main(String[] args) {
+		
+	}
+}
