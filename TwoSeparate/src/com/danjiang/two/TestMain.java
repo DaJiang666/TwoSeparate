8952c52f406c97cb4308c433acf36f5515911855
Administrator <Administrator@MS-20110711NYGU>
2016-06-12 00:22:56 +0800

    1

--- TwoSeparate/src/com/danjiang/two/TestMain.java
+++ TwoSeparate/src/com/danjiang/two/TestMain.java
@@ -1,4 +1,6 @@
 package com.danjiang.two;
+
+
 /**
  * 
  * Author: ZhangDanJiang
@@ -6,10 +8,29 @@ package com.danjiang.two;
  * Date:2016年6月12日 Time:上午12:03:06
  * 
  * Function:
+ *   This is TwoSeparate search 
+ *   这是一个二分查找 的Java示例
  * 
  */
 public class TestMain {
+	
+	
 	public static void main(String[] args) {
+		//创建当前类对象
+		TestMain test = new TestMain();
+		test.todo();
+	}
+	
+	/***
+	 * 创建一个飞静态方法
+	 */
+	private void todo() {
+		// 顶一个有序数组   m 属于数组中的一个 查询m在数组中的位置
+		int[] arr = {1,2,3,4,5,8,9,15,16,18,23,27,29};
+		// 要查询的值
+		int m = 23;
+		
+		
 		
 	}
 }
